849b387eef71c8501e99b9caa174f587b7343868
Cehhiro <hiro@cehh.io>
2019-01-10 20:10:25 -0600

    promise attr

--- src/index.ts
+++ src/index.ts
@@ -5,33 +5,37 @@
 
 import Web3              from 'web3';
 import Contract          from 'web3/eth/contract';
+import BN                from 'bn.js';
 import { ABIDefinition } from "web3/eth/abi";
-
-import { ERC20Abi } from './abi/erc20Abi';
+import { ERC20Abi }      from './abi/erc20Abi';
 
 const web3 = new Web3('https://mainnet.infura.io/metamask');
 
 interface IInfo {
-  name:         string | undefined;
-  symbol:       string | undefined;
-  totalSupply:  number | undefined;
+  name:         Promise<string> | undefined;
+  symbol:       Promise<string> | undefined;
+  totalSupply:  Promise<number> | undefined;
+  decimals:     Promise<number> | undefined;
 }
 
 export class ERC20 {
-  address:  string | undefined;
-  token:    Contract | undefined;
-  info:     IInfo;
-  erc20Abi: ABIDefinition[];
+  address:        string | undefined;
+  token:          Contract | undefined;
+  info:           IInfo;
+  erc20Abi:       ABIDefinition[];
+  decimalFactor:  BN;
 
   constructor(address: string) {
-    this.address  = undefined;
-    this.token    = undefined;
-    this.erc20Abi = ERC20Abi;
+    this.address       = undefined;
+    this.token         = undefined;
+    this.erc20Abi      = ERC20Abi;
+    this.decimalFactor = web3.utils.toBN('1');
 
     this.info = {
       name:         undefined,
       symbol:       undefined,
       totalSupply:  undefined,
+      decimals:     undefined,
     }
 
     
@@ -39,37 +43,52 @@ export class ERC20 {
       this.address = address;
       this.token   = new web3.eth.Contract(this.erc20Abi, address);
 
-      this.token.methods.name().call()
-        .then((name: string) => this.info.name = name)
-        .catch((e: Error) => console.error("Could not get name", e))
-
-      this.token.methods.symbol().call()
-        .then((symbol: string) => this.info.symbol = symbol)
-        .catch((e: Error) => console.error("Could not get symbol", e))
-
-      this.token.methods.totalSupply().call()
-        .then((totalSupply: number) => this.info.totalSupply = totalSupply)
-        .catch((e: Error) => console.error("Could not get total supply", e))
+      this.info.name        = this.token.methods.name().call();
+      this.info.symbol      = this.token.methods.symbol().call();
+      this.info.totalSupply = this.token.methods.totalSupply().call();
+      this.info.decimals    = this.token.methods.decimals().call();
+      
+      this.info.decimals.then((decimals: number) => {
+        this.decimalFactor = web3.utils.toBN('1' + '0'.repeat(decimals));
+      })
     }
   }
 
   getBalance = (user: string): (Promise<any> | undefined) => {
-    return this.token ? this.token.methods.balanceOf(user).call() : undefined;
+    if(this.token !== undefined) {
+      return new Promise((resolve, reject) => {
+        this.token.methods.balanceOf(user).call()
+          .then((balance: string) => {
+            const b = web3.utils.toBN(balance);
+            const w = b.div(this.decimalFactor);
+            const f = b.mod(this.decimalFactor);
+            
+            resolve(`${w}.${f}`);
+          })
+          .catch((e: Error) => reject(e));
+      })
+    } else {
+      return undefined;
+    }
   }
 
   getInfo = () : IInfo => {
     return this.info;
   }
 
-  getName = () : (string | undefined) => {
+  getName = () : (Promise<string> | undefined) => {
     return this.info.name;
   }
   
-  getSymbol = (): (string | undefined) => {
+  getSymbol = (): (Promise<string> | undefined) => {
     return this.info.symbol;
   }
   
-  getTotalSupply = (): (number | undefined) => {
+  getTotalSupply = (): (Promise<number> | undefined) => {
     return this.info.totalSupply;
+  }
+  
+  getDecimals = (): (Promise<number> | undefined) => {
+    return this.info.decimals;
   }
 }
