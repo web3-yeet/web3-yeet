873764cb1d4d693ff311d90ee9f76a1ac4dd2358
Cehhiro <hiro@cehh.io>
2019-01-11 23:23:52 -0600

    signatures

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,7 @@ import { Wallet } from './wallet';
 const t = new ERC20('0xB8c77482e45F1F44dE1745F52C74426C631bDD52');
 const w = new Wallet();
 
-w.sendERC20('',1,t);
-*/
+w.signMessage("hi").then(s => {
+  w.checkMessage("hi",s)
+}).catch(console.error)
+ */
--- src/wallet.ts
+++ src/wallet.ts
@@ -7,6 +7,7 @@ import Web3                   from 'web3';
 import BN                     from 'bn.js';
 import { ERC20 }              from './erc20';
 import { TransactionReceipt } from 'web3/eth/../types';
+import { Tx }                 from 'web3/eth/types';
 
 const web3 = new Web3(Web3.givenProvider || 'https://mainnet.infura.io/metamask');
 
@@ -16,77 +17,95 @@ interface IReceipt {
 }
 
 export class Wallet {
-  address: Promise<string[]>;
+  addressList: Promise<string[]>;
 
   constructor() {
-    this.address = web3.eth.getAccounts();
+    this.addressList = web3.eth.getAccounts();
   }
 
   getAddress = async (): Promise<string | undefined> => {
-    const addressList: string[] = await this.address;
+    const addressList: string[] = await this.addressList.catch((e: Error) => { throw(e) } );
     return addressList[0];
   }
 
   sendEther = async (address: string, amount: number): Promise<IReceipt> => {
     const sender = await this.getAddress().catch((e: Error) => { throw(e) } );
 
-    return new Promise((resolve, reject) => {
-      if(typeof sender !== 'string')
-        reject("There is no wallet access.");
+    if(typeof sender !== 'string')
+      Promise.reject("There is no wallet access.");
 
-      const wallet = sender as string;
-      const wei    = web3.utils.toWei(`${amount}`, 'ether');
+    const wallet = sender as string;
+    const wei    = web3.utils.toWei(`${amount}`, 'ether');
 
-      return web3.eth.sendTransaction({
-        from:   wallet,
-        to:     address,
-        value:  wei.toString(),
-      })
-        .on('receipt', (receipt: TransactionReceipt) => {
-          resolve({
-            success: receipt.status,
-            txhash: receipt.transactionHash
-          });
-        })
-        .on('error', (error: any) => {
-          throw error;
-        });
-    });
+    const rawTx: Tx = {
+      from:   wallet,
+      to:     address,
+      value:  wei.toString(),
+    };
+
+    return this.sendTransaction(rawTx);
   }
 
-  sendERC20 = async (address: string, amount: number, erc20: ERC20) => {
+  sendERC20 = async (address: string, amount: number, erc20: ERC20): Promise<IReceipt> => {
     const sender        = await this.getAddress().catch((e: Error) => { throw(e) } );
-    const decimalFactor = await erc20.getDecimalFactor();
+    const decimalFactor = await erc20.getDecimalFactor().catch((e: Error) => { throw(e) } );
 
+    if(typeof erc20.token === 'undefined')
+      Promise.reject("Token instance couldn't be initialised");
+
+    if(typeof sender !== 'string')
+      Promise.reject("There is no wallet access.");
+
+    if(!web3.utils.isBN(decimalFactor))
+      Promise.reject("Could not get token data.");
+
+    const wallet = sender as string;
+    const wei    = web3.utils.fromWei(web3.utils.toBN(web3.utils.toWei(amount.toString(), 'ether')).mul(decimalFactor as BN), 'ether');
+
+    const rawTx: Tx = {
+      from:   wallet,
+      to:     erc20.address,
+      value:  "0",
+      data:   (erc20.token as any).methods.transfer(address, wei.toString()).encodeABI()
+    };
+
+    return this.sendTransaction(rawTx);
+  }
+
+  signMessage = async (msg: string): Promise<string> => {
+    const signer = await this.getAddress().catch((e: Error) => { throw(e) } );
+
+    if(typeof signer !== 'string')
+      return Promise.reject("There is no wallet access.");
+
+    return (web3.eth.personal.sign(msg, signer as string) as any);
+  }
+  
+  checkMessage = async (msg: string, signature: string): Promise<boolean> => {
+    const signer    = await this.getAddress().catch((e: Error) => { throw(e) } );
+
+      // web3.personal.ecRecover is not in @types
+    const recovered = await (web3.eth.personal.ecRecover(msg, signature) as any).catch((e: Error) => { throw(e) } );
+    
+    if(typeof signer !== 'string')
+      Promise.reject("There is no wallet access.");
+ 
+
+    return (signer as string).toLowerCase() === recovered.toLowerCase();
+  }
+
+  private sendTransaction = (rawTx: Tx): Promise<IReceipt> => {
     return new Promise((resolve, reject) => {
-      if(typeof erc20.token === 'undefined')
-        reject("Token instance couldn't be initialised");
-
-      if(typeof sender !== 'string')
-        reject("There is no wallet access.");
-
-      if(!web3.utils.isBN(decimalFactor))
-        reject("Could not get token data.");
-
-      const wallet = sender as string;
-      const wei    = web3.utils.fromWei(web3.utils.toBN(web3.utils.toWei(amount.toString(), 'ether')).mul(decimalFactor as BN), 'ether');
-      const rawTx  = {
-        from:   wallet,
-        to:     erc20.address,
-        value:  "0",
-        data:   (erc20.token as any).methods.transfer(address, wei.toString()).encodeABI()
-      };
-
-      return web3.eth.sendTransaction(rawTx)
+      web3.eth.sendTransaction(rawTx)
         .on('receipt', (receipt: TransactionReceipt) => {
           resolve({
-            success: receipt.status,
-            txhash: receipt.transactionHash
+            success:  receipt.status,
+            txhash:   receipt.transactionHash
           });
         })
         .on('error', (error: any) => {
-          throw error;
+          throw(error);
         });
-    });
+    })
   }
 }
