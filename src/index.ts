b4340177afd0bd8c12818afe507234f6bcd5541b
Cehhiro <hiro@cehh.io>
2019-01-13 17:08:32 -0600

    rebuild index

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,2 @@
 export { ERC20 } from './erc20';
 export { Wallet } from './wallet';
-
-import { ERC20 } from './erc20';
-import { Wallet } from './wallet';
-
-const t = new ERC20('0xB8c77482e45F1F44dE1745F52C74426C631bDD52');
-const w = new Wallet();
-
-w.getAddress().then(console.log)
